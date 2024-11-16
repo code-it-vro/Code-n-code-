6eefba5e1877372ca5f8cda21941acba66c2f8a3
code-it-vro <ashutosh2001shukla@gmail.com>
2024-11-16 13:45:10 +0530

    adding typescript code also in find the power of k subarray1

--- Arrays/Sliding-Window/findThePowerOfKSubarrays1/findThePowerOfKSubarray1.js
+++ Arrays/Sliding-Window/findThePowerOfKSubarrays1/findThePowerOfKSubarray1.js
@@ -34,3 +34,45 @@ var resultsArray = function (nums, k) {
   // Return the final result array
   return answer;
 };
+
+
+
+
+
+
+
+/////////////////////////// TypeScript solution Below  ////////////////////////////
+
+/**
+ * @param {number[]} nums - The input array of integers
+ * @param {number} k - The size of the subarray
+ * @return {number[]} - The power of all subarrays of size k
+ */
+// const resultsArray = (nums: number[], k: number): number[] => {
+//   const answer: number[] = [];
+
+//   /**
+//    * Helper function to check if an array is consecutive.
+//    * @param {number[]} arr - A subarray of `nums`.
+//    * @return {boolean} - True if the array is consecutive, false otherwise.
+//    */
+//   function isConsecutive(arr: number[]): boolean {
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] !== arr[i - 1] + 1) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//   // Iterate through the array to evaluate each subarray of size `k`
+//   for (let i = 0; i <= nums.length - k; i++) {
+//     const tempArray: number[] = nums.slice(i, i + k);
+//     if (isConsecutive(tempArray)) {
+//       answer.push(Math.max(...tempArray));
+//     } else {
+//       answer.push(-1);
+//     }
+//   }
+//   return answer;
+// };
